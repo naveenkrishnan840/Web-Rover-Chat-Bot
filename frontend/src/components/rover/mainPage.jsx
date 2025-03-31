6cfe9ff0b9a6d84770cba1852031b97d8052b97e
naveenkrishnan840 <58537261+naveenkrishnan840@users.noreply.github.com>
2025-03-31 22:37:17 +0530

    Update mainPage.jsx

--- frontend/src/components/rover/mainPage.jsx
+++ frontend/src/components/rover/mainPage.jsx
@@ -14,7 +14,7 @@ export default function Home() {
     
     try {
       console.log('Attempting to connect...');
-      const response = await fetch('http://65.1.139.145:8081/setup-browser', {
+      const response = await fetch('http://65.1.139.145:8082/setup-browser', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
